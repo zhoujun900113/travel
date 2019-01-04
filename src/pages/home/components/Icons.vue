2d39e4f33cd1a62a73dd6e25924d7162b7c939f1
zhoujun900113 <437709293@qq.com>
2019-01-04 14:23:06 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src= "item.imgUrl"/>
+                    </div>
+                    <p class = "icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+    name:"HomeIcons",
+    data: function(){
+        return{
+            IconList:[{
+                id: '0001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+                desc: '景点门票景点门票景点门票景点门票'
+            },{
+                id: '0002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+                desc: '一日游'
+            },{
+                id: '0003',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+                desc: '杭州必游'
+            },{
+                id: '0004',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+                desc: '动植物园'
+            },{
+                id: '0005',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f6/e54fad3ea337b02.gif',
+                desc: '年终大促'
+            },{
+                id: '0006',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/17/99402a22ce4af302.png',
+                desc: '西湖'
+            },{
+                id: '0007',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+                desc: '泡温泉'
+            },{
+                id: '0008',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
+                desc: '西溪湿地'
+            },{
+                id: '0009',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/17/99402a22ce4af302.png',
+                desc: '西湖'
+            }],
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                loop:true
+            },
+        }
+    },
+    computed:{
+        pages(){
+            const pages = []
+            this.IconList.forEach((item,index) => {
+                const page = Math.floor(index/8)
+                if(!pages[page]){
+                    pages[page] = []
+                }
+                pages[page].push(item)
+            })
+            return pages
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import "~styles/varibles.styl"
+    @import "~styles/mixins.styl"
+    .icons >>> .swiper-container
+        height: 0
+        padding-bottom: 50%
+    .icons
+        overflow:hidden
+        height: 0
+        padding-bottom:100%
+        width: 100%
+        .icon
+            position: relative
+            overflow: hidden
+            height: 0
+            width: 25%
+            padding-bottom: 25%
+            float: left 
+            .icon-img
+                position: absolute
+                top: 0
+                left: 0
+                right: 0
+                bottom: 0.44rem
+                box-sizing: border-box
+                padding: 0.1rem
+                .icon-img-content
+                    display:block
+                    margin: 0 auto
+                    height: 100%
+            .icon-desc
+                position:absolute
+                left: 0
+                right: 0
+                bottom: 0
+                line-height:0.44rem
+                height: 0.44rem
+                color: $darkTextColor
+                text-align: center
+                ellipsis()
+</style>

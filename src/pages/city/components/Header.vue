e1f5c7585253155a459d8f64f78a8c19827ce6e5
zhoujun900113 <437709293@qq.com>
2019-01-07 14:46:33 +0800

    add router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to='/'>
+            <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
+    </div>
+
+</template>
+<script>
+export default {
+    name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        overflow: hidden
+        position: relative
+        height: $headerHeight
+        line-height:$headerHeight
+        text-align:center
+        color:#FFF
+        background : $bgColor
+        font-size :.32rem
+        .header-back
+            position:absolute
+            width: .64rem
+            text-align : center
+            color:#FFF
+            top: 0
+            left: 0
+            font-size: .4rem
+</style>
